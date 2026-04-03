6ae9d51a34428fcafd3057c69401d9894363b593
meilaa <meilasari656@gmail.com>
2026-04-03 23:27:45 +0700

    LabSession3 DBMS Integration and Function Based Views

--- Smart City/iet_2026_24782082/iet_2026_24782082/settings.py
+++ Smart City/iet_2026_24782082/iet_2026_24782082/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'iet_2026_24782082.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartcity_db',
+        'USER': 'postgres',
+        'PASSWORD': 'meila656',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- Smart City/iet_2026_24782082/main_app/models.py
+++ Smart City/iet_2026_24782082/main_app/models.py
@@ -1,3 +1,14 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+

7133e5c05f0d5a762f4f31db55c3f006ab9ea0ee
meilaa <meilasari656@gmail.com>
2026-05-20 13:35:15 +0700

    Langkah 1 Instalasi dan Setup JWT

--- Smart City/iet_2026_24782082/iet_2026_24782082/settings.py
+++ Smart City/iet_2026_24782082/iet_2026_24782082/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'usermanagement_24782082',
     'dashboard_24782082',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer'
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}

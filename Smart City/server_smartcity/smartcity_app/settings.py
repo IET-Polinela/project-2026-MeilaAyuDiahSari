cff1bd6144458d630f6239c86d060823feffce70
meilaa <meilasari656@gmail.com>
2026-06-10 17:20:35 +0700

    Deploy frontend SPA to GitHub Pages

--- Smart City/server_smartcity/smartcity_app/settings.py
+++ Smart City/server_smartcity/smartcity_app/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'smartcity_app.urls'
